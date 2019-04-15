be06b217ac90893bd87ea3cfdd98eb22e6ee1159
Jackson Coleman <jcoleman32897@gmail.com>
2019-04-15 12:31:49 -0700

    Key words are now extracted from the responses and stored into the words table

---
+++ FeedbackWebApp/FeedbackWebApp/KeyWords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeedbackWebApp
+{
+    public class KeyWords
+    {
+        private string keyWord;
+        private string responseID;
+
+        public string KeyWord { get => keyWord; set => keyWord = value; }
+        public string ResponseID { get => responseID; set => responseID = value; }
+    }
+}
